5ccdaf6c351a79855ec73f34c2eb0aa4df628a51
zhangjinfei <1246001152@qq.com>
2019-04-03 15:12:57 +0800

    添加学生类

---
+++ src/com/idea/entity/Student.java
@@ -0,0 +1,13 @@
+package com.idea.entity;
+
+/**
+ * @author : 张晋飞
+ * date : 2019/4/3
+ */
+public class Student {
+
+    private Integer id;
+    private String name;
+    private Integer age;
+
+}
