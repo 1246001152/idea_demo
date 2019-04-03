30825b6dae2b0bd98fd0337a4ba74c9a555d498b
zhang-jinfei <china_zhangjf@163.com>
2019-04-03 15:29:48 +0800

    添加老师类

---
+++ src/com/idea/entity/Teacher.java
@@ -0,0 +1,14 @@
+package com.idea.entity;
+
+/**
+ * @author : 张晋飞
+ * date : 2019/4/3
+ */
+public class Teacher {
+
+    private Integer id;
+    private String name;
+    private Integer age;
+
+
+}
